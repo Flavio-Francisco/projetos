bb9d9082e6468a81a8bd4a38fff693eb9fbf2f42
Flavio <Flavio>
2023-09-04 16:11:08 -0300

    erro

--- todobackend/app/Models/TodoListModelUser.php
+++ todobackend/app/Models/TodoListModelUser.php
@@ -20,5 +20,19 @@ class TodoListModelUser extends Model
 
     ];
 
-
+// Validation
+protected $validationRules      = [];
+protected $validationMessages   = [];
+protected $skipValidation       = false;
+protected $cleanValidationRules = true;
+  // Callbacks
+  protected $allowCallbacks       = true;
+  protected $beforeInsert         = [];
+  protected $afterInsert          = [];
+  protected $beforeUpdate         = [];
+  protected $afterUpdate          = [];
+  protected $beforeFind           = [];
+  protected $afterFind            = [];
+  protected $beforeDelete         = [];
+  protected $afterDelete          = [];
 }
