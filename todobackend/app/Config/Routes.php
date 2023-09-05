4ca6470487c4c9ceba7450fb67819494035ed474
Flavio Francisco <flavyobass151@gmail.com>
2023-09-05 07:48:16 -0300

    retornando tokem

--- todobackend/app/Config/Routes.php
+++ todobackend/app/Config/Routes.php
@@ -36,7 +36,7 @@ $routes->post('/auth', 'User::auth');
 $routes->post('/query/(:segment)', 'User::show/$1');
 $routes->put('/update/(:segment)', 'User::update/$1');
 $routes->put('/delete/(:segment)', 'User::delete/$1');
-$routes->post('/login', 'Login::auth');
+$routes->post('/login', 'Login::authJwt');
 
         #Routes 
  $routes->get('/taskget', 'Task::get');
--- todobackend/app/Controllers/Login.php
+++ todobackend/app/Controllers/Login.php
@@ -4,24 +4,41 @@ namespace App\Controllers;
 use App\Models\TodoListModelUser;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
+use Firebase\JWT\JWT;
 
 
 class Login extends ResourceController{
 
 use ResponseTrait;
 
+//where('name',$query->name )->first()
+public function __construct(){
 
-
-public function auth(){
+    $this->model =  new TodoListModelUser();
     
-    $model = new TodoListModelUser();
+}
+
+public function authJwt(){
+    
+  
 
     $query = $this->request->getJSON();
-    $data = $model->where('name',$query->name )->first();
+    $data = $this->model->where('name',$query->name )->first();
 
     if($data['name'] ==$query->name && $data['password'] == $query->password){
 
-        return $this->respond($data);
+        $payload =[
+            // temp de expiração
+            "exp"=>time() + 3600,
+            //data de criação
+            "iat"=> time(),
+            //dados do usuario
+            "data" =>$data  
+        ];
+
+        $encode= JWT::encode($payload, $_ENV['KEY'],"HS256");
+
+        return $this->respond($encode);
 
     }
 
