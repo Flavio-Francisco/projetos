8bdc8f8a535490e57e55805945522ad33de57e66
Flavio <Flavio>
2023-09-08 17:02:22 -0300

    começando inerface Category

---
+++ app/src/context/Category.tsx
@@ -0,0 +1,16 @@
+
+
+
+interface CategoryProps {
+    id: number,
+    name: string,
+    icon: string,
+    task_id: number,
+}
+
+
+
+interface AuthContextDataProps {
+category:CategoryProps,
+
+}
